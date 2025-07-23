4452ec0d22e93a7418cccddec13687763137af10
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-07-23 12:06:30 +0530

    we have defined all access modifiers now -updated

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,25 @@
 "use strict";
 class Person {
-    //defaultly it is reffered as public in Typescript
     constructor(email, name) {
         //if we dont want it to be accessible to anyone
         this.city = "delhi"; // Initialized
+        //defaultly it is reffered as public in Typescript
+        this._courseCount = 1;
+        this._courses = 1;
         this.email = email;
         this.name = name;
         this.city; //its accessible here ,only with in the class
+    }
+}
+//extended class (Inheritence)
+class SubPerson extends Person {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        //this._courseCount = 4; // ❌ Error: private property access not allowed
+        this._courses = 4; //we can use protected within class and as well as inherited class
     }
 }
 const krish = new Person('k@v.com', 'krishna');
